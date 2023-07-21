9e920034a63b23ef6f089dae826b5f7c7a385b6d
BihanMathieu <mathieu.bihan35@gmail.com>
2023-07-21 10:07:52 +0200

    first commit

---
+++ src/main/java/controller/ShowDept.java
@@ -0,0 +1,67 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modele.DeptPair;
+import service.NumDeptMapper;
+
+/**
+ * Servlet implementation class ShowDept
+ */
+@WebServlet("/ShowDept")
+public class ShowDept extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowDept() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+        String departement = request.getParameter("departement");
+
+        departement = departement.trim();
+
+        String address;
+
+        if (departement.isEmpty()) {
+            address = "/WEB-INF/results/missing-num.jsp";
+        } else {
+            NumDeptMapper deptMapper = new NumDeptMapper();
+            String nomDept = deptMapper.findDept(departement);
+
+            DeptPair deptPair = new DeptPair(departement, nomDept);
+            
+            if (nomDept != null) {
+                request.setAttribute("deptPair", deptPair);
+                address = "/WEB-INF/results/show-departement.jsp";
+            } else {
+                request.setAttribute("deptPair", deptPair);
+                address = "/WEB-INF/results/unknown-num.jsp";
+            }
+        }
+        request.getRequestDispatcher(address).forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/enter-dept.jsp
@@ -0,0 +1,88 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Chercher un departement</title>
+<style type="text/css">
+* {
+	margin: 0;
+	padding: 0;
+}
+
+body::before {
+	content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-image: url('asset/France_départementale.png');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	opacity: 0.0;
+	z-index: -1;
+}
+
+h1 {
+	color: #007BFF;
+	text-align: center;
+	padding-top: 30px;
+}
+
+form {
+	max-width: 400px;
+	margin: 20px auto;
+	padding: 20px;
+	background-color: #fff;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+	box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+label {
+	display: block;
+	margin-top: 0px;
+	margin-bottom: 10px;
+	font-weight: bold;
+	margin-bottom: 10px;
+}
+
+input[type="text"] {
+	width: 100%;
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	box-sizing: border-box;
+}
+
+input[type="submit"] {
+	display: block;
+	width: 100%;
+	margin-top: 20px;
+	padding: 10px;
+	background-color: #007BFF;
+	color: #fff;
+	border: none;
+	border-radius: 3px;
+	cursor: pointer;
+	transition: background-color 0.3s;
+}
+
+input[type="submit"]:hover {
+	background-color: #0056b3;
+}
+</style>
+</head>
+<body>
+	<h1>Entrez votre département</h1>
+	<form action="ShowDept" method="POST">
+		<label for="departement">Département :</label> <input type="text"
+			id="departement" name="departement"> <br> <input
+			type="submit" value="Envoyer">
+	</form>
+
+</body>
+</html>
